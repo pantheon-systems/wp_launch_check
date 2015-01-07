17c338f25b93b2ca09b5af21b5e0250a7ae85732
Mike Van Winkle <mike@mikevanwinkle.com>
2015-01-07 00:31:22 +0000

    Build script

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-git clone git@github.com:wp-cli/wp-cli.git /tmp/wp-cli
-composer update -d /tmp/wp-cli/
-
-sudo cp /tmp/wp-cli/bin/wp /usr/local/bin/wp
-sudo chmod +x /usr/local/bin/wp
-export WP_CLI_ROOT=/tmp/wp-cli
+sudo composer update
 phpunit
