f95ccabce883050599aef16076f2ce090c11bfaf
Mike Van Winkle <mike@mikevanwinkle.com>
2015-01-12 22:49:33 +0000

    Fixes unit test

--- tests/test-searcher.php
+++ tests/test-searcher.php
@@ -3,7 +3,7 @@ use \Pantheon\Utils;
 use \Pantheon\Filesearcher;
 use \Pantheon\Messenger;
 
-class SampleTest extends PHPUnit_Framework_TestCase {
+class SearcherTest extends PHPUnit_Framework_TestCase {
 
 	function testFileSearch() {
 		// replace this with some actual testing code
