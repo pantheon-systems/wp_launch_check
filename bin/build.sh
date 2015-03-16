f537f1d46d08298cc475cae4402b0d51315c78c0
Mike Van Winkle <mike@mikevanwinkle.com>
2015-03-16 22:47:13 +0000

    fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 WORKINGDIR=$PWD
 sudo git clone https://github.com/wp-cli/wp-cli.git /tmp/wp-cli
+if [ ! -d /tmp/wp-cli ]; then
+	cd /tmp/wp-cli
+	sudo composer update
+fi
+
 cd /tmp/wp-cli
-
-sudo composer update
-
 sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ /tmp/wp-cli/php/
-
 sudo php -dphar.readonly=0 utils/make-phar.php wp-cli.phar --quiet
-
 sudo cp wp-cli.phar $WORKINGDIR/
 
 if [[ ! $( type s3cmd ) ]]; then 
@@ -51,5 +51,7 @@ urlencoding_mode = normal
 use_https = False
 verbosity = WARNING' > ~/.s3cfg
 
-s3cmd put --acl-public wp-cli.phar s3://pantheon-wp-cli/release-test.phar
+version=$( cat $WORKINGDIR/VERSION )
+wp_version=$( cat /tmp/wp-cli/VERSION )
+s3cmd put --acl-public wp-cli.phar s3://wp-cli/$wp_version-lc-$version.phar
 
