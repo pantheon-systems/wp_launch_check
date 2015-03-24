6ffe81246d316c9dfc646e6fe8a4329318692d22
mikevanwinkle <mike@mikevanwinkle.com>
2015-03-24 23:02:12 +0000

    One more try to get the build right

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 WORKINGDIR=$PWD
-CLI_DIR=/tmp/wp-cli
+CLI_DIR=$WORKINGDIR/wp-cli
 sudo git clone https://github.com/wp-cli/wp-cli.git $CLI_DIR
-
-cd $CLI_DIR
-sudo curl -sS https://getcomposer.org/installer | php
-sudo chmod +x composer.phar
-sudo php composer.phar update
+sudo composer update --working-dir=$CLI_DIR
 sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ $CLI_DIR/php/
+cd $CLI_DIR
 sudo php -dphar.readonly=0 utils/make-phar.php $WORKINGDIR/wp-cli.phar --quiet
 cd $WORKINGDIR
