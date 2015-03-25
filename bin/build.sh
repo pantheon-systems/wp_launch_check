00420ba6d68f0d1f212cb8b98100a897be5627e4
Michael Van Winkle <mike@mikevanwinkle.com>
2015-03-25 10:45:25 -0700

    Debugging composer failure

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,13 @@
 WORKINGDIR=$PWD
 CLI_DIR=~/wp-cli
 sudo git clone https://github.com/wp-cli/wp-cli.git $CLI_DIR
-sudo composer update --working-dir=$CLI_DIR
+echo -n "COMPOSER: $(whereis composer)"
+if [ -f composer.phar ]
+then 
+	php ./composer.phar update --working-dir=$CLI_DIR --prefer-dist
+else 
+	composer update --working-dir=$CLI_DIR --prefer-dist 
+fi
 sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ $CLI_DIR/php/
 cd $CLI_DIR
 sudo php -dphar.readonly=0 utils/make-phar.php $WORKINGDIR/wp-cli.phar --quiet
