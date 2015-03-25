b90d3503df52d76a3840e547ec34be4aba224a88
Michael Van Winkle <mike@mikevanwinkle.com>
2015-03-25 10:05:40 -0700

    Trying to get the build right

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WORKINGDIR=$PWD
-CLI_DIR=$WORKINGDIR/wp-cli
+CLI_DIR=~/wp-cli
 sudo git clone https://github.com/wp-cli/wp-cli.git $CLI_DIR
 sudo composer update --working-dir=$CLI_DIR
 sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ $CLI_DIR/php/
