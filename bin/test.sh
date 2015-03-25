db62e29252385a98de968b709aadd416ae1d16f5
Mike Van Winkle <mike@mikevanwinkle.com>
2015-03-25 18:42:36 +0000

    Move phar building to the test script

--- bin/test.sh
+++ bin/test.sh
@@ -2,3 +2,13 @@
 whereis composer
 if [ -f composer.phar ]; then php ./composer.phar update; else composer update; fi
 phpunit
+
+WORKINGDIR=$PWD
+CLIDIR=/home/travis/wp-cli
+sudo git clone 
+sudo composer update --working-dir=$CLI_DIR --prefer-dist
+sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ $CLI_DIR/php/
+cd $CLI_DIR
+sudo php -dphar.readonly=0 utils/make-phar.php $WORKINGDIR/wp-cli.phar --quiet
+cd $WORKINGDIR
+
