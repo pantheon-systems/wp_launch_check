bfaa237e6ae29d75997adb69cf42591fd90c5595
Mike Van Winkle <mike@mikevanwinkle.com>
2015-03-25 20:03:51 +0000

    Testing the build script

--- bin/test.sh
+++ bin/test.sh
@@ -5,8 +5,9 @@ phpunit
 
 WORKINGDIR=$PWD
 CLIDIR=/home/travis/wp-cli
-sudo git clone 
-sudo composer update --working-dir=$CLI_DIR --prefer-dist
+sudo git clone https://github.com/wp-cli/wp-cli wp-cli
+ARGS="--working-dir=$CLI_DIR --prefer-dist"
+if [ -f composer.phar ]; then php ./composer.phar update $ARGS; else composer update $ARGS; fi
 sudo rsync --exclude=.git -avzu $WORKINGDIR/php/ $CLI_DIR/php/
 cd $CLI_DIR
 sudo php -dphar.readonly=0 utils/make-phar.php $WORKINGDIR/wp-cli.phar --quiet
