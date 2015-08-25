4495fa495c8d2e9e59a61210d83acb61a18d4efe
Ronan Dowling <ronan@gortonstudios.com>
2015-08-25 13:19:06 -0500

    Add more complete checking for invalid cron jobs
    Added warnings for overdue and invalid cron schedule times. Also added a warning for lots of scheduled cron jobs and removed the raw cron info from the output

--- php/pantheon/checks/cron.php
+++ php/pantheon/checks/cron.php
@@ -7,7 +7,8 @@ use Pantheon\Messenger;
 use Pantheon\View;
 
 class Cron extends Checkimplementation {
- 
+  const MAX_CRON_DISPLAY = 50;
+
   public function init() {
     $this->name = 'cron';
     $this->action = 'No action required';
@@ -44,53 +45,96 @@ class Cron extends Checkimplementation {
   }
 
   public function checkCron() {
-    $this->cron = get_option('cron'); 
+    $total = 0;
+    $invalid = 0;
+    $overdue = 0;
+    $now = time();
+
+    $this->cron_rows = array();
+    $cron = get_option('cron');
+
+    // Count the cron jobs and alert if there are an excessive number scheduled.
+    foreach ($cron as $timestamp => $crons) { 
+      foreach ($crons as $job => $data) {
+        $class = 'ok';
+        $data = array_shift($data);
+
+        // If this is an invalid timestamp.
+        if (!is_int($timestamp) || $timestamp == 0) {
+          $invalid++;
+          $class = "error";
+          $next = 'INVALID';
+        }
+        // If the timestamp is in the past.
+        else if ($timestamp < $now) {
+          $past++;
+          $class = "error";
+          $next = date('M j,Y @ H:i:s', $timestamp) . ' (PAST DUE)';
+        }
+        $this->cron_rows[] = array('class' => $class, 'data' => array('jobname' => $job, 'schedule' => $data['schedule'], 'next' => $next));
+        $total++;
+      }
+    }
+
+    if ($invalid) {
+      $this->alerts[] = array( 
+        'class' => 'fail', 
+        'message' => "You have $invalid cron job(s) with an invalid time.", 
+        'code' => 2
+      );
+    }    
+    if ($overdue) {
+      $this->alerts[] = array( 
+        'class' => 'pass', 
+        'message' => "You have $past cron job(s) which are past due. Make sure that cron jobs are running on your site.", 
+        'code' => 1 
+      );
+    }    
+    if ($total > self::MAX_CRON_DISPLAY) {
+      $this->alerts[] = array( 
+        'class' => 'pass', 
+        'message' => "You have $total cron jobs scheduled. This is too many to display and may indicate a problem with your site.", 
+        'code' => 1 
+      );
+      // Truncate the output.
+      // @TODO: Put a note next to the output table reiterating that these are not the full results.
+      $this->cron_rows = array_splice($this->cron_rows, 0, self::MAX_CRON_DISPLAY);
+    }
   }
 
   public function message(Messenger $messenger) {
-      if (!empty($this->alerts)) {
-        $total = 0;
-        $rows = array();
-        foreach ($this->alerts as $alert) {
-          $total += $alert['code'];
-          $label = 'info';
-          if (1 === $alert['code']) {
-            $label = 'warning';
-          } elseif( 2 >= $alert['code']) {
-            $label = 'error';
-          }
-          $rows[] = array(
-            'message' => $alert['message'],
-            'class' => $label
-          );
+    if (!empty($this->alerts)) {
+      $total = 0;
+      $rows = array();
+      foreach ($this->alerts as $alert) {
+        $total += $alert['code'];
+        $label = 'info';
+        if (1 === $alert['code']) {
+          $label = 'warning';
+        } elseif( 2 >= $alert['code']) {
+          $label = 'error';
         }
-        
-        $avg = $total/count($this->alerts);
-        $this->result = sprintf("%s\n%s", $this->description, View::make('checklist', array('rows'=> $rows)));
-        // format the cron table
-        $rows = array();
+        $rows[] = array(
+          'message' => $alert['message'],
+          'class' => $label
+        );
+      }
+      
+      $avg = $total/count($this->alerts);
+      $this->result = sprintf("%s\n%s", $this->description, View::make('checklist', array('rows' => $rows)));
+      
+      // format the cron table
+      $rows = array();
+      if ($this->cron_rows) {
         $headers = array( 
           'jobname' => 'Job',
           'schedule' => 'Frequency',
           'next'    => 'Next Run',
         );
-
-        // @TODO move this logic to the run() function or checkCron() function
-				if ($this->cron) {
-        	foreach ($this->cron as $timestamp => $crons) { 
-          	foreach ($crons as $job => $data) {
-	            $class = 'ok';
-  	          $data = array_shift($data);
-    	        if ($timestamp < time()) {
-        	      $class = "error";
-      	        $this->action = "<div class='warning'>Some cronjobs are outdated.</div>";
-          	  }
-            	$rows[] = array('class'=>$class,'data'=> array('jobname' => $job,'schedule' => $data['schedule'], 'next' => date('M j,Y @ H:i:s', $timestamp)));    
-          	}
-        	}
-				}
-        $this->result .= sprintf( "<hr/>%s",View::make('table', array('rows'=>$rows,'headers'=>$headers)));
+        
+        $this->result .= sprintf( "<hr/>%s",View::make('table', array('rows' => $this->cron_rows, 'headers' => $headers)));
         $this->score = $avg;
+      }
     }
     $messenger->addMessage(get_object_vars($this));
   }
