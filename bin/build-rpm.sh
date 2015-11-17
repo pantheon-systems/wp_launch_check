9e6b85b442453ab680a22e2e524270bf50cee72c
Daniel Bachhuber <daniel@handbuilt.co>
2015-11-17 09:35:34 -0800

    Update rpm build for new distribution

--- bin/build-rpm.sh
+++ bin/build-rpm.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # requires fpm (`gem install fpm`)
 
-name='wp-cli'
+name='wp-launch-check'
 version=$(cat VERSION)
 iteration="$(date +%Y%m%d%H%M).git$(git rev-parse --short HEAD)"  # datecode + git sha-ref: "201503020102.gitef8e0fb"
 arch='x86_64'
-url='https://github.com/pantheon-systems/wp-launch-check' # custom plugin for wp-cli
+url='https://github.com/pantheon-systems/wp_launch_check'
 vendor='Pantheon'
-description='Custom compiled version of wp-cli for use on Pantheon'
-install_prefix='/opt/wp-cli'
+description='Distribution version of wp-launch-check'
+install_prefix='/opt/wp-launch-check'
 
 fpm -s dir -t rpm  \
     --name "${name}" \
@@ -19,4 +19,4 @@ fpm -s dir -t rpm  \
     --vendor "${vendor}" \
     --description "${description}" \
     --prefix "$install_prefix" \
-    wp-cli.phar
+    wp-launch-check.phar
