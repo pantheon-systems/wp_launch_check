4d886279e07852b3916e0660100b60a87eaeb0a3
Daniel Bachhuber <daniel@handbuilt.co>
2017-09-20 18:37:14 -0700

    Correctly check `WP_CONTENT_DIR` for `object-cache.php`, not `ABSPATH`
    Includes tests

--- php/pantheon/checks/objectcache.php
+++ php/pantheon/checks/objectcache.php
@@ -23,7 +23,7 @@ class Objectcache extends Checkimplementation {
 
 	public function run() {
 		global $redis_server;
-		$object_cache_file = ABSPATH.'/wp-content/object-cache.php';
+		$object_cache_file = WP_CONTENT_DIR . '/object-cache.php';
 		if (!file_exists($object_cache_file)) {
 			$this->alerts[] = array("message"=> "No object-cache.php exists", "code" => 1);
 		} else {
@@ -31,7 +31,7 @@ class Objectcache extends Checkimplementation {
 		}
 
 		if (empty($redis_server)) {
-			$this->alerts[] = array("message"=> "Using redis with WP-Redis object caching would speed up your backend.", "code" => 1);
+			$this->alerts[] = array("message"=> 'Use Redis with the WP Redis object cache drop-in to speed up your backend. <a href="https://pantheon.io/docs/wordpress-redis/" target="_blank">Learn More</a', "code" => 1);
 		} else {
 			$this->alerts[] = array("message"=> "Redis found", "code" => 0);
 		}
