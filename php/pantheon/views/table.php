2f62f59d39fc7af4dbf6e7f95b13ab82b6ad0f97
tesladethray <tesladethray+github@gmail.com>
2019-09-06 13:14:34 -0700

    Sanitized output going to Pantheon dashboard

--- php/pantheon/views/table.php
+++ php/pantheon/views/table.php
@@ -3,16 +3,16 @@
 			<tr>
 				<?php if(isset($headers)): ?>
 					<?php foreach ($headers as $header): ?>
-						<th><?php echo $header; ?></th>
+						<th><?php echo htmlspecialchars($header); ?></th>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</tr>
 	</thead>
 	<tbody>
 			<?php foreach($rows as $row): ?>
-				<tr class="<?php if(isset($row['class'])) { echo $row['class']; } ?>">
+				<tr class="<?php if(isset($row['class'])) { echo htmlspecialchars($row['class']); } ?>">
 					<?php foreach($row['data'] as $values): ?>
-						<td><?php echo $values; ?></td>
+						<td><?php echo htmlspecialchars($values); ?></td>
 					<?php endforeach; ?>
 				</tr>
 			<?php endforeach; ?>
