86a659ead8e5481d989ae4a92d63922473f4b648
Zhenzhen You <youzhenzhen@gmail.com>
2019-10-09 16:19:38 -0700

    Do not need htmlspecialchars for the rest good links in our code

--- php/pantheon/views/checklist.php
+++ php/pantheon/views/checklist.php
@@ -1,5 +1,5 @@
 <ul class="check-list">
 <?php foreach($rows as $row): ?>
-	<li class="severity-<?php echo htmlspecialchars($row['class']); ?>"><p class="result"><?php echo htmlspecialchars($row['message']); ?></p></li>
+	<li class="severity-<?php echo $row['class']; ?>"><p class="result"><?php echo $row['message']; ?></p></li>
 <?php endforeach; ?>
 </ul>
