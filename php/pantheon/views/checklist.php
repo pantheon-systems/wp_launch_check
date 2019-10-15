8a516d64c94cc587add76f7d91882beb9f75aa0c
Zhenzhen You <youzhenzhen@gmail.com>
2019-10-15 13:56:02 -0700

    Use wp_kses_post and esc_attr instead of htmlspecialchars

--- php/pantheon/views/checklist.php
+++ php/pantheon/views/checklist.php
@@ -1,5 +1,7 @@
 <ul class="check-list">
 <?php foreach($rows as $row): ?>
-	<li class="severity-<?php echo htmlspecialchars($row['class']); ?>"><p class="result"><?php echo htmlspecialchars($row['message']); ?></p></li>
+	<li class="severity-<?php echo esc_attr($row['class']); ?>">
+		<p class="result"><?php echo wp_kses_post($row['message']); ?></p>
+	</li>
 <?php endforeach; ?>
 </ul>
--- php/pantheon/views/table.php
+++ php/pantheon/views/table.php
@@ -3,16 +3,16 @@
 			<tr>
 				<?php if(isset($headers)): ?>
 					<?php foreach ($headers as $header): ?>
-						<th><?php echo htmlspecialchars($header); ?></th>
+						<th><?php echo wp_kses_post($header); ?></th>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</tr>
 	</thead>
 	<tbody>
 			<?php foreach($rows as $row): ?>
-				<tr class="<?php if(isset($row['class'])) { echo htmlspecialchars($row['class']); } ?>">
+				<tr class="<?php if(isset($row['class'])) { echo esc_attr($row['class']); } ?>">
 					<?php foreach($row['data'] as $values): ?>
-						<td><?php echo htmlspecialchars($values); ?></td>
+						<td><?php echo wp_kses_post($values); ?></td>
 					<?php endforeach; ?>
 				</tr>
 			<?php endforeach; ?>
