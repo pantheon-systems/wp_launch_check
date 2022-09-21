b3ac0ebf5e0294cef122a8684888eb4f1c106590
Chris Reynolds <chris@jazzsequence.com>
2022-09-21 11:14:02 -0600

    add no action required message as the fallback

--- php/pantheon/checks/sessions.php
+++ php/pantheon/checks/sessions.php
@@ -17,7 +17,7 @@ class Sessions extends Checkimplementation {
 		$this->has_plugin = class_exists("Pantheon_Sessions");
 		// If the plugin was not found, define the recommended action.
 		// Otherwise, we don't want to recommend anything, we're all good here.
-		$this->action = ! $this->has_plugin ? 'You should install the Native PHP Sessions plugin - https://wordpress.org/plugins/wp-native-php-sessions/' : null;
+		$this->action = ! $this->has_plugin ? 'You should install the Native PHP Sessions plugin - https://wordpress.org/plugins/wp-native-php-sessions/' : 'No action required';
 
 		return $this;
 	}
