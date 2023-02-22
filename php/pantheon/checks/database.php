c27e6c42e51bfffdd379033d1ed79888eef170ca
John Spellman <john.spellman@pantheon.io>
2023-02-22 13:58:31 -0700

    Use wp->prefix to get table prefix

--- php/pantheon/checks/database.php
+++ php/pantheon/checks/database.php
@@ -44,7 +44,7 @@ class Database extends Checkimplementation {
 		global $wpdb;
 		foreach ( $this->getTables() as $table ) {
 			$this->tables[$table->TABLE_NAME] = $table;
-			if ( preg_match( "#.*options#s", $table->TABLE_NAME ) ) { 
+			if ( $wpdb->prefix . 'options' ) { 
 				$options_table = $table;
 				break;
 			}
