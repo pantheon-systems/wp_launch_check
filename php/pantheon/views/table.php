acf948676b689e13d28bef27ff7d7d1fa175cb0c
John Spellman <john.spellman@pantheon.io>
2023-05-05 13:23:47 -0600

    Pass table values through our santization functions

--- php/pantheon/views/table.php
+++ php/pantheon/views/table.php
@@ -1,9 +1,13 @@
+<?php 
+
+use Pantheon\Utils; ?>
+
 <table class='table table-condensed'>
 	<thead>
 			<tr>
 				<?php if(isset($headers)): ?>
 					<?php foreach ($headers as $header): ?>
-						<th><?php echo $header; ?></th>
+						<th><?php echo Utils::sanitize_data( $header ); ?></th>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</tr>
@@ -12,7 +16,7 @@
 			<?php foreach($rows as $row): ?>
 				<tr class="<?php if(isset($row['class'])) { echo $row['class']; } ?>">
 					<?php foreach($row['data'] as $values): ?>
-						<td><?php echo $values; ?></td>
+						<td><?php echo Utils::sanitize_data( $values ); ?></td>
 					<?php endforeach; ?>
 				</tr>
 			<?php endforeach; ?>
