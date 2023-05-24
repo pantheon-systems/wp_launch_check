15426ba32f0958c53946399a033fc969a091a5b7
Chris Reynolds <chris@jazzsequence.com>
2023-05-24 13:38:42 -0600

    remove unused function

--- php/pantheon/checks/themes.php
+++ php/pantheon/checks/themes.php
@@ -92,34 +92,6 @@ class Themes extends Checkimplementation {
 	}
 
 	/**
-	 * Get a WordPress vulnerability API token if one is defined and we're in the right environment.
-	 * Copied from wp_launch_check/php/pantheon/checks/plugins.php
-	 * Uses the WPSCAN_API_TOKEN constant if defined.
-	 *
-	 * @return string|false
-	 * @todo Replace this with a Patchstack API token.
-	 */
-	protected function getWpVulnApiToken() {
-		if ( defined( 'WPSCAN_API_TOKEN' ) ) {
-			// Don't use WPSCAN if PANTHEON_WPSCAN_ENVIRONMENTS have not been specified.
-			if( ! defined( 'PANTHEON_WPSCAN_ENVIRONMENTS' ) ) {
-				return '';
-			}
-
-			$environments = ( ! is_array( PANTHEON_WPSCAN_ENVIRONMENTS ) ) ? explode( ',', PANTHEON_WPSCAN_ENVIRONMENTS ) : PANTHEON_WPSCAN_ENVIRONMENTS;
-
-			// Only run WPSCAN on the specified environments unless it's been configured to run on all (*).
-			if ( in_array( getenv( 'PANTHEON_ENVIRONMENT' ), $environments, true ) || in_array( '*', $environments, true ) ) {
-				return WPSCAN_API_TOKEN;
-			}
-		}
-
-		// TODO: Replace this PANTHEON_WPVULNDB_API_TOKEN with a new Patchstack API token.
-		// return getenv( 'PANTHEON_WPVULNDB_API_TOKEN' );
-		return '';
-	}
-
-	/**
 	 * Checks the theme slug against the vulnerability db
 	 * @param $theme_slug string (required) string representing the theme slug
 	 *
