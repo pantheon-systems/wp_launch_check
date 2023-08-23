7510efe672a0453c25d4148fdbb797fc8dde75cc
Rachel Whitton <rachel@pantheon.io>
2023-08-23 11:07:10 -0500

    Prettier

--- php/pantheon/checks/objectcache.php
+++ php/pantheon/checks/objectcache.php
@@ -31,8 +31,7 @@ class Objectcache extends Checkimplementation {
 		}
 		if ( defined( 'WP_REDIS_OBJECT_CACHE' )) {
 			$this->alerts[] = array('message'=> 'WP Redis for object caching was found. We recommend using Object Cache Pro as a replacement. <a href="https://docs.pantheon.io/guides/object-cache-pro/installing-configuring/" target="_blank">Learn More</a> about how to install Object Cache Pro.', 'code' => 1);
-		}
-		elseif ( ! defined( 'WP_REDIS_CONFIG' ) || ! WP_REDIS_CONFIG ) {
+		} elseif ( ! defined( 'WP_REDIS_CONFIG' ) || ! WP_REDIS_CONFIG ) {
 			$this->alerts[] = array("message"=> 'Use Object Cache Pro to speed up your backend. <a href="https://docs.pantheon.io/guides/object-cache-pro/installing-configuring/" target="_blank">Learn More</a>', "code" => 1);
 		} else {
 			$this->alerts[] = array("message"=> "Object Cache Pro found", "code" => 0);
