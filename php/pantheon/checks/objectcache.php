a34b1cfd240ccbfc6f87839d86fa7f2f5628d8ac
Rachel <rachel@pantheon.io>
2023-08-23 10:35:23 -0500

    Check for WP Redis that recommends OCP when found
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>

--- php/pantheon/checks/objectcache.php
+++ php/pantheon/checks/objectcache.php
@@ -30,6 +30,9 @@ class Objectcache extends Checkimplementation {
 			$this->alerts[] = array("message"=> "object-cache.php exists", "code" => 0);
 		}
 
+		} else {
+			$this->alerts[] = array('message'=> 'WP Redis for object caching was found. We recommend using Object Cache Pro as a replacement. <a href="https://docs.pantheon.io/guides/object-cache-pro/installing-configuring/" target="_blank">Learn More</a> about how to install Object Cache Pro.', 'code' => 1);
+		}
 		if ( ! defined( 'WP_REDIS_CONFIG' ) || ! WP_REDIS_CONFIG ) {
 			$this->alerts[] = array("message"=> 'Use Object Cache Pro to speed up your backend. <a href="https://docs.pantheon.io/guides/object-cache-pro/installing-configuring/" target="_blank">Learn More</a>', "code" => 1);
 		} else {
